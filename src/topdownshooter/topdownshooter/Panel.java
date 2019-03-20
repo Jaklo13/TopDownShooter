dd38f917f57bde46db6b8533454a2e711a268bf5
TomasWilson <vonstuckrad@gmail.com>
2019-03-20 18:47:11 +0100

    Update Filepaths and cleaned up optionmenu

---
+++ src/topdownshooter/topdownshooter/Panel.java
@@ -0,0 +1,160 @@
+package topdownshooter;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.Point;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.geom.AffineTransform;
+import java.awt.geom.Line2D;
+import java.awt.geom.Point2D;
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+import java.util.ConcurrentModificationException;
+import javax.swing.BorderFactory;
+import javax.swing.JPanel;
+
+public class Panel extends JPanel{
+    private Image backgroundImage = createImage (0,0);
+    private Point2D.Float dim;
+
+    public Panel (int x, int y) {
+        dim = new Point2D.Float (x,y);
+        setFocusable (true);
+        setBackground (Color.LIGHT_GRAY);
+        setBorder (BorderFactory.createLineBorder(Color.BLACK, 10));
+        setPreferredSize(new Dimension ((int)dim.x,(int)dim.y));
+        
+        MA ma = new MA ();
+        addMouseListener (ma);
+        addMouseMotionListener (ma);
+        addMouseWheelListener (ma);
+        addKeyListener (new KL ());
+    }
+    
+    public void paintBackgroundImage (Graphics2D g) {
+        g.drawImage(backgroundImage, 0,0, null);
+    }
+    
+    public void paintGameObjects (Graphics2D g) {
+        try {
+            ArrayList<GameObject> gObjects = GameManager.GM.getGameObjects();
+            for (GameObject go : gObjects) {
+                if (!(go instanceof Wall)) {
+                    BufferedImage sprite = go.getSprite();
+                    float halfWidth = (float)go.getBounds().getWidth() / 2, halfHeight = (float)go.getBounds().getHeight() / 2;
+                    Point2D.Float pos = go.getPos(GameObject.CENTER);
+                    AffineTransform at = new AffineTransform ();
+
+                    at.translate(pos.getX(), pos.getY());                           //First the Affine Transform is centerd on the position
+                    at.rotate(go.getRotation());                                    //Then it's rotated and centered around the Object's position
+                    at.translate(-halfWidth, -halfHeight);                          
+                    g.drawImage(go.getSprite(), at, this);
+
+//                    g.setColor(Color.red);                                        //use this to see the hitboxes of all Objects                        
+//                    g.drawRect((int)go.bounds.x, (int)go.bounds.y, (int)go.bounds.width, (int)go.bounds.height);
+                }
+            }
+        } catch (ConcurrentModificationException e) {
+            System.out.println (e + ", paintObjects");
+        }
+    }
+    
+    public void paintBullets (Graphics2D g) {
+        BufferedImage bulletSprite = GameManager.GM.getSprite(GameManager.BULLET_SPRITES, 0);
+        float width = (float)bulletSprite.getWidth(), halfHeight = (float)bulletSprite.getHeight() / 2, thickness = 0.5f;
+        Shot[] shots = new Shot[GameManager.GM.getShotHandler().getShots().size()];
+        GameManager.GM.getShotHandler().getShots().toArray(shots);
+        Line2D.Float l;
+        AffineTransform at;
+        
+        for (Shot shot : shots) {
+            l = shot.getTrace();
+            at = new AffineTransform ();
+            at.translate(l.getX1(), l.getY1());
+            at.rotate(l.x2 - l.x1, l.y2 - l.y1);
+            at.translate(0, -halfHeight * thickness);
+            at.scale(Math.hypot(l.x2 - l.x1, l.y2 - l.y1) / width, thickness);
+            g.drawImage(shot.getSprite (), at, this);
+        }
+    } 
+    
+    public void paintUI (Graphics2D g) {
+        ArrayList<Player> players = GameManager.GM.getPlayers();
+        if (players.isEmpty())
+            return;
+        Point ulc = new Point (10,10); //upperLeftCorner
+        int borderSize = 10;
+        int gap = 60;
+        int healthBarLength = ((int)dim.x - (players.size() * borderSize * 2 + gap * (players.size() - 1) + ulc.x * 2)) / (players.size ()), healthBarHeight = 15;
+        
+        for (int i = 0; i < players.size(); i++) {
+            g.setColor(new Color (100,100,100,100));
+            g.fillRect(ulc.x + i * (borderSize * 2 + healthBarLength + gap), ulc.y, borderSize * 2 + healthBarLength,healthBarHeight + borderSize * 2);
+            g.setColor(Player.PLAYER_COLORS[players.get(i).getPn()]);
+            g.fillRect(ulc.x + borderSize + i * (borderSize * 2 + healthBarLength + gap), ulc.y + borderSize, (int)(GameManager.GM.getPlayerHealthPercentage(i) * healthBarLength),healthBarHeight);
+        }
+    }
+    
+    //for testing and debugging
+    public void paintDebug (Graphics2D g) {
+
+    }
+
+    public void paintEverything (Graphics2D g) {
+        try { 
+            paintBackgroundImage (g);
+            paintGameObjects (g);
+            paintBullets (g);
+            paintUI (g);
+            paintDebug (g);
+
+        } catch (NullPointerException e) {
+            System.out.println(e + ", paintEverything");
+        }
+
+        repaint ();
+    }
+
+    @Override
+    public void paintComponent (Graphics g) {
+        try {
+            Image dbImage = createImage (getWidth(),getHeight());
+            paintEverything ((Graphics2D)dbImage.getGraphics());
+            g.drawImage(dbImage, 0, 0, this);
+        } catch (Exception e) {
+            System.out.println (e + ", paintComponent");
+        }
+    }
+    
+    public void setBackgroundImage (Image b) {
+        backgroundImage = b;
+    }
+    
+    public class MA extends MouseAdapter {
+        @Override
+        public void mousePressed (MouseEvent e) {
+            GameManager.GM.mouseClicked ();
+        }
+        @Override
+        public void mouseReleased (MouseEvent e) {
+            GameManager.GM.mouseReleased ();
+        }
+    }
+    
+    public class KL extends KeyAdapter {
+        @Override
+        public void keyPressed(KeyEvent e) {
+            GameManager.GM.addKp(e.getKeyCode());
+        }
+        @Override
+        public void keyReleased(KeyEvent e) {
+            GameManager.GM.removeKp(e.getKeyCode());
+        }
+    }
+}
---
+++ src/topdownshooter/topdownshooter/Shot.java
@@ -0,0 +1,101 @@
+package topdownshooter;
+
+import java.awt.geom.Line2D;
+import java.awt.geom.Point2D;
+import java.awt.image.BufferedImage;
+
+public class Shot {
+    public static final float MAX_TRACE_LENGTH = 5;     //times the hitLine length
+    public static final float TRACE_SPEED = 0.8f;       //how fast the trail is compared to the shot
+    private ShotHandler sh;
+    private GameObject shooter;
+    private BufferedImage sprite;
+    private Line2D.Float hitLine, trace;
+    private Point2D.Float endPoint;
+    private boolean isMoving = true;
+    private int dmg;
+
+    
+    public Shot (ShotHandler sh, Line2D.Float hitLine, GameObject shooter, int dmg) {
+        this.sh = sh;
+        this.hitLine = hitLine;
+        this.shooter = shooter;
+        this.trace = new Line2D.Float (hitLine.x1,hitLine.y1,hitLine.x2,hitLine.y2);
+        this.dmg = dmg;
+        if (shooter instanceof Player) {
+            int pn = ((Player)shooter).getPn () + 1;
+            this.sprite = ((Player)shooter).getBullet ();
+        } else {
+            this.sprite = GameManager.GM.getSprite(GameManager.BULLET_SPRITES, 0);
+        }
+        
+    }
+    
+    public void update () {
+        move ();
+    }
+    
+    public void move () {
+        float d1 = hitLine.x2 - hitLine.x1, d2 = hitLine.y2 - hitLine.y1; 
+        if (isMoving) {
+            hitLine.x1 += d1;
+            hitLine.x2 += d1;
+            hitLine.y1 += d2;
+            hitLine.y2 += d2;
+        }
+        
+        if (isMoving) {
+            trace.x2 = hitLine.x2;
+            trace.y2 = hitLine.y2;
+        } else if (endPoint != null) {
+            trace.x2 = endPoint.x;
+            trace.y2 = endPoint.y;
+        } else
+            System.out.println ("Error, no endPoint");
+        
+        if (!hasReachedMaxTrailLength()) {
+            d1 *= TRACE_SPEED;
+            d2 *= TRACE_SPEED;
+        }
+        trace.x1 += d1;
+        trace.y1 += d2;
+        
+    }
+    
+    public boolean hasReachedMaxTrailLength () {
+        return (Math.hypot(hitLine.x2 - hitLine.x1, hitLine.y2 - hitLine.y1) * MAX_TRACE_LENGTH < Math.hypot(trace.x2 - trace.x1, trace.y2 - trace.y1));
+    }
+    
+    public boolean tooShort () {
+        return (Math.hypot(hitLine.x1 - hitLine.x2, hitLine.y1 - hitLine.y2) > Math.hypot(trace.x1 - trace.x2, trace.y1 - trace.y2));
+    }
+    
+    public boolean isMoving () {
+        return isMoving;
+    }
+    
+    public void stopMoving (Point2D.Float p) {
+        endPoint = p;
+        isMoving = false;
+    }
+
+    public Line2D.Float getHitLine() {
+        return hitLine;
+    }
+
+    public Line2D.Float getTrace() {
+        return trace;
+    }
+
+    public GameObject getShooter() {
+        return shooter;
+    }
+
+    public BufferedImage getSprite() {
+        return sprite;
+    }
+
+    public int getDmg() {
+        return dmg;
+    }
+}
